275ef7b0322a52b9f8c143acec4e99e9aa6a9b16
Carmen Kaouk <Carmen.Kaouk@bighand.com>
2023-11-02 06:47:30 -0700

    Endpoint Middleware done

---
+++ TaskMangementProject/Middlewares/EndpointMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Presentation.Support;
+using RequestResponse;
+using System.Reflection;
+using RequestResponse.Enums;
+
+namespace Presentation.Middlewares;
+
+public class EndpointMiddleware : Middleware
+{
+    public override Response ProcessRequest(FileContext fileContext)
+    {
+        var controllerInstance = ((object?)fileContext.data["ControllerInstance"]);
+        var parameters = ((object[])fileContext.data["Parameters"]);
+        var targetMethod = ((MethodInfo)fileContext.data["MethodInfo"]);
+        var result = targetMethod.Invoke(controllerInstance, parameters);
+        object responseContent = result;
+        if (result is Task taskResult)
+        {
+            taskResult.GetAwaiter().GetResult();
+            responseContent = taskResult;
+        }
+        Response response = new Response { RequestId = ((Request)fileContext.data["Request"]).RequestId, StatusCode = StatusCodes.Success, Content = responseContent };
+        return response;
+    }
+}
