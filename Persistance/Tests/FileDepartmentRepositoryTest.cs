184a12fee7ff0278d39b6ec4576063f7c41bd22f
adamm777 <adamalmoustafa.mrouwe@lau.edu>
2023-11-02 13:57:15 +0200

    File department add test created

---
+++ Persistance/Tests/FileDepartmentRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Application.Models;
+using Moq;
+using NUnit.Framework;
+using Persistence.Interfaces;
+using Persistence.Repositories;
+
+namespace Persistence.Tests
+{
+    [TestFixture]
+    public class FileDepartmentRepositoryTest
+    {
+        private Mock<AbstractDatabaseManager> databaseManagerMock;
+        private FileDepartmentRepository departmentRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            databaseManagerMock = new Mock<AbstractDatabaseManager>();
+            departmentRepository = new FileDepartmentRepository(databaseManagerMock.Object);
+        }
+
+        [Test]
+        public void AddDepartmentTest()
+        {
+            var department = new Department { Id = 1, Name = "It" };
+            
+            departmentRepository.Add(department);
+           
+            Assert.Contains(department, databaseManagerMock.Object.Departments);
+        }
+    }
+}
